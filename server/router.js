93606a156ef0996c1278f6e375bdced1021a2fce
Reva <111722589+rssathe08311@users.noreply.github.com>
2025-03-09 13:52:00 -0700

    brainstorming image generation

--- server/router.js
+++ server/router.js
@@ -24,6 +24,10 @@ const router = (app, openai) => {
   app.post('/chat', (req, res) => {
     controllers.chat.getChatResponse(req, res, openai);
   });
+  //add a /image post enpoint that will communicate with imageController in controllers
+  //ver 1 code in chat
+  //need to create a button for testing currently and then latter will figure out smother integration with player experience
+  //will have to do a similar thing with audio.
 };
 
 module.exports = router;
